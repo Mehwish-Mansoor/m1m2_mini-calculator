ae5c92baa7432c562f1ea150ca665b2dfe65f955
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-30 10:00:00 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 #! user/env node
-import inquirer from "inquirer";
-const answer = await inquirer.prompt([
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const inquirer_1 = __importDefault(require("inquirer"));
+const answer = await inquirer_1.default.prompt([
     {
         message: "enter first number",
         type: "number",
